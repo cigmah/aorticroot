60cdacde3c36b9560171d2abc905c5fe27ea3fee
Jamin Wu <Jamin.Wu07@gmail.com>
2019-06-15 18:42:30 +1000

    Flattened JSON return of question.

---
+++ README.md
@@ -0,0 +1,11 @@
+# AORTIC ROOT
+
+Backend API for AORTA.
+
+# TODOs
+
+- [ ] Make a fork of drfpasswordless with randomly generated username
+- [ ] Add QuestionResponse table
+- [ ] Check question creation - how?
+- [ ] Get random question with spaced repetition if authenticated, or just any
+- [ ] Add case tables
--- questions/serializers.py
+++ questions/serializers.py
@@ -8,34 +8,47 @@ from django.contrib.auth.models import User
 
 class QuestionCommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(source='user_id')
+
     class Meta:
         model = QuestionComment
         fields = ('user', 'content', 'timestamp')
 
 class QuestionLikeSerializer(serializers.ModelSerializer):
     user = UserSerializer(source='user_id')
+
     class Meta:
         model = QuestionLike
         fields = ('user',)
 
 class QuestionTagSerializer(serializers.ModelSerializer):
     tag = TagSerializer(source='tag_id')
+
     class Meta:
         model = QuestionTag
         fields = ('tag',)
 
+    def to_representation(self, instance):
+        original = super().to_representation(instance)
+        return original.pop('tag')
+
 class QuestionDistractorSerializer(serializers.ModelSerializer):
     distractor = ChoiceSerializer(source='choice_id')
+
     class Meta:
         model = QuestionDistractor
         fields = ('distractor',)
+
+    def to_representation(self, instance):
+        original = super().to_representation(instance)
+        return original.pop('distractor')
 
 class QuestionSerializer(serializers.ModelSerializer):
     tags = QuestionTagSerializer(many=True)
     distractors = QuestionDistractorSerializer(many=True)
     comments = QuestionCommentSerializer(many=True)
     likes = QuestionLikeSerializer(many=True)
-    author = UserSerializer(source='user_id')
+    user_id = UserSerializer()
+    answer = ChoiceSerializer()
 
     class Meta:
         model = Question
