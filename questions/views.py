04f9008c56a2339fa8203447a9406ef96ca9732f
Jamin Wu <Jamin.Wu07@gmail.com>
2019-06-15 22:34:06 +1000

    Added question response view.

--- questions/views.py
+++ questions/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from rest_framework import filters
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from tags.models import Tag
 from choices.models import Choice
@@ -74,3 +75,8 @@ class QuestionCommentDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = QuestionComment.objects.all()
     serializer_class = QuestionCommentSerializer
+
+class QuestionResponse(generics.ListCreateAPIView):
+    queryset = QuestionResponse.objects.all().order_by('-timestamp')
+    serializer_class = QuestionResponseSerializer
+    permission_classes = (permissions.IsAuthenticated)
